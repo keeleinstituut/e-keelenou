0bb07ca249cf936effd2baa755e9111ad5f12c1f
redmine <git@keeleleek.ee>
2015-06-26 08:55:27 +0000

    Lisatud OpenSearch päringuprotokoll

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ include_once 'plib/redirect.php';
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, height=device-height, user-scalable=no">
 		<meta name="eknid" content="<?php echo $uid; ?>">
+	        <link rel="search" type="application/opensearchdescription+xml" title="EKI e-keelenõu" href="eki_kn.xml">
 		<!--script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js'></script-->
 		<script type='text/javascript' src='lib/jquery.js'></script>
 
